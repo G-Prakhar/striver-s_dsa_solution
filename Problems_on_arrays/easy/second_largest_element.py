6c17fbd1e8650d8684d85dbb93f9986d97a4dbbd
Prakhar Gupta <prak.gupt1948@gmail.com>
2026-09-17 22:02:51 +0530

    changes made

---
+++ Problems_on_arrays/easy/second_largest_element.py
@@ -0,0 +1,23 @@
+def secondlargensmall(arr: List[int]) -> int:
+    largest = arr[0]
+    secondlargest = float('-inf')
+    smallest = arr[0]
+    secondsmallest = float('inf')
+
+    for i in range(len(arr)):
+        if arr[i] > largest:
+            secondlargest = largest
+            largest = arr[i]
+        elif arr[i] < largest and arr[i] > secondlargest:
+            secondlargest = arr[i]
+
+    for i in range(len(arr)):
+        if arr[i] < smallest:
+            secondsmallest = smallest
+            smallest = arr[i]
+        elif arr[i] > smallest and arr[i] < secondsmallest:
+            secondsmallest = arr[i]
+
+    print(secondsmallest, secondlargest)    
+
+secondlargensmall([1, 2, 4, 7, 7, 5])
---
+++ basics/easy_n_medium_patterns/pattern6.py
@@ -0,0 +1,9 @@
+n = 5
+num = 0
+
+for i in range(1, n + 1):
+    num = num*10 + i
+    
+for i in range(1, n + 1):
+    print(num)
+    num = num//10
